9c5b59de32724d1765b41995e3805769db7dd406
Luiz <luiz.f_ferraresi@hotmail.com>
2024-03-15 23:33:33 -0300

    remove import

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -2,8 +2,6 @@ from typing import Dict, Any
 
 from aws_lambda_powertools.utilities.typing.lambda_context import LambdaContext
 
-from src.helpers.lambda_event import CognitoEvent
-
 
 def lambda_handler(event: Dict[str, Any], context: LambdaContext) -> Dict[str, Any]:
     print(f"PoolID: {event['userPoolId']} | ClientID: {event['callerContext']['clientId']} | UserName: {event['userName'][:15]}")
